1b8e273c738b553486e6c6de115bb34f210d62f0
Anish <anishanish0102@gmail.com>
2024-02-20 23:29:58 +0530

    first commit

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 let myDate =  document.querySelector("#date");
     
 setInterval(()=>{
@@ -119,3 +121,24 @@ setInterval(()=>{
 },1000)
 
 
+
+
+// Drodown
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+ 
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+myFunction()
